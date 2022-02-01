cec84803d0a0c30c8afa28d7584ed19e5b2a13df
OpenOrg-gg <adam@openorg.gg>
2022-02-01 17:32:28 -0500

    initial

---
+++ tests/conftest.py
@@ -0,0 +1,90 @@
+import pytest
+from brownie import config
+from brownie import Contract
+
+
+@pytest.fixture
+def gov(accounts):
+    yield accounts.at("0xf4d2888d29d722226fafa5d9b24f9164c092421e", force=True)
+
+
+@pytest.fixture
+def user(accounts):
+    yield accounts[0]
+
+
+@pytest.fixture
+def rewards(accounts):
+    yield accounts[1]
+
+
+@pytest.fixture
+def guardian(accounts):
+    yield accounts[2]
+
+
+@pytest.fixture
+def management(accounts):
+    yield accounts[3]
+
+
+@pytest.fixture
+def strategist(accounts):
+    yield accounts[4]
+
+
+@pytest.fixture
+def keeper(accounts):
+    yield accounts[5]
+
+
+@pytest.fixture
+def token():
+    token_address = "0x6b175474e89094c44da98b954eedeac495271d0f"  # this should be the address of the ERC-20 used by the strategy/vault (DAI)
+    yield Contract(token_address)
+
+
+@pytest.fixture
+def amount(accounts, token, user):
+    amount = 10_000 * 10 ** token.decimals()
+    # In order to get some funds for the token you are about to use,
+    # it impersonate an exchange address to use it's funds.
+    reserve = accounts.at("0xf4d2888d29d722226fafa5d9b24f9164c092421e", force=True)
+    token.transfer(user, amount, {"from": reserve})
+    yield amount
+
+
+@pytest.fixture
+def weth():
+    token_address = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
+    yield Contract(token_address)
+
+
+@pytest.fixture
+def weth_amout(user, weth):
+    weth_amout = 10 ** weth.decimals()
+    user.transfer(weth, weth_amout)
+    yield weth_amout
+
+
+@pytest.fixture
+def vault(pm, gov, rewards, guardian, management, token):
+    Vault = pm(config["dependencies"][0]).Vault
+    vault = guardian.deploy(Vault)
+    vault.initialize(token, gov, rewards, "", "", guardian, management)
+    vault.setDepositLimit(2 ** 256 - 1, {"from": gov})
+    vault.setManagement(management, {"from": gov})
+    yield vault
+
+
+@pytest.fixture
+def strategy(strategist, keeper, vault, Strategy, gov):
+    strategy = strategist.deploy(Strategy, vault)
+    strategy.setKeeper(keeper)
+    vault.addStrategy(strategy, 10_000, 0, 2 ** 256 - 1, 1_000, {"from": gov})
+    yield strategy
+
+
+@pytest.fixture(scope="session")
+def RELATIVE_APPROX():
+    yield 1e-5
